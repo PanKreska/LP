3aabe67fa315b024a7465048d0a57eb2b72a6d8b
PanKreska <pawel.cacko@wp.pl>
2022-09-02 12:19:09 +0200

    prefix change

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
                 use: [
                     "style-loader",
                     "css-loader",
-                    "sass-loader"
+                    "sass-loader",
+                    // "postcss-loader"
                 ],
             },
             {
