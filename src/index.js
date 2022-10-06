452e90eb54bdc934d56823945c3facbbd6ee6ed1
PanKreska <pawel.cacko@wp.pl>
2022-10-06 17:09:44 +0200

    Slider implementation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import  './style.scss';
+
+new Swiper(".swiper", {
+    effect: 'slide',
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    bulletClass: 'swiper-pagination-bullet-active',
+  },
+
+  navigation: {
+    nextEl: '.right',
+    prevEl: '.left',
+  },
+  pagination: {
+    el: '.dots',
+    currentClass : '.active',
+    clickable: 'true',
+  },
+})
